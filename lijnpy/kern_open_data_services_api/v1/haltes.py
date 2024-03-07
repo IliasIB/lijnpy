bd3580ee31d186b107cf1ccf17b9ff2a390ce235
Ilias Bachiri <ilias.bachiri@gmail.com>
2024-03-07 17:09:45 +0100

    Added haltes module Caveat: dropped support for haltesleutels since they aren't properly defined

---
+++ lijnpy/kern_open_data_services_api/v1/haltes.py
@@ -0,0 +1,233 @@
+from pydantic_core import ValidationError
+
+from lijnpy import _logger
+from lijnpy.exceptions import DeLijnAPIException
+from lijnpy.kern_open_data_services_api.v1 import _rest_adapter
+from lijnpy.models import (
+    Dienstregeling,
+    Doorkomst,
+    DoorkomstNota,
+    GeoCoordinaat,
+    Halte,
+    HalteInDeBuurt,
+    Omleiding,
+    RealTimeDoorkomst,
+    Richting,
+    RitNota,
+    Storing,
+)
+from lijnpy.utils import clean_doorkomst, clean_halte, clean_halte_in_de_buurt
+
+
+def get_haltes() -> list[Halte]:
+    """Get a list of all available stops
+
+    Returns:
+        list[Halte]: A list of all available stops
+
+    Raises:
+        DeLijnAPIException: If the API request fails
+    """
+    result = _rest_adapter.get("/haltes")
+    try:
+        assert result.data is not None
+        haltes = [Halte(**clean_halte(halte)) for halte in result.data["haltes"]]
+    except (AssertionError, ValidationError) as e:
+        _logger.error(f"Failed to parse the response from the API: {e}")
+        raise DeLijnAPIException from e
+
+    return haltes
+
+
+def get_haltes_in_neighbourhood(geo_coordinaat: GeoCoordinaat) -> list[HalteInDeBuurt]:
+    """Get a list of all available stops in the neighbourhood of the given geo-coordinates
+
+    Args:
+        geo_coordinaat (GeoCoordinaat): The geo-coordinates to search around
+
+    Returns:
+        list[Halte]: A list of all available stops in the neighbourhood of the given geo-coordinates
+
+    Raises:
+        DeLijnAPIException: If the API request fails
+    """
+    result = _rest_adapter.get(
+        f"/haltes/indebuurt/{geo_coordinaat.latitude},{geo_coordinaat.longitude}",
+    )
+    try:
+        assert result.data is not None
+        haltes = [
+            HalteInDeBuurt(**clean_halte_in_de_buurt(halte))
+            for halte in result.data["haltes"]
+        ]
+    except (AssertionError, ValidationError) as e:
+        _logger.error(f"Failed to parse the response from the API: {e}")
+        raise DeLijnAPIException from e
+
+    return haltes
+
+
+def get_halte(entiteitnummer: str, haltenummer: str) -> Halte:
+    """Get the stop with the given entity and stop number
+
+    Args:
+        entiteitnummer (str): The number of the entity
+        haltenummer (str): The number of the stop
+
+    Returns:
+        Halte: The stop with the given entity and stop number
+
+    Raises:
+        DeLijnAPIException: If the API request fails
+    """
+    result = _rest_adapter.get(f"/haltes/{entiteitnummer}/{haltenummer}")
+    try:
+        assert result.data is not None
+        halte = Halte(**clean_halte(result.data))
+    except (AssertionError, ValidationError) as e:
+        _logger.error(f"Failed to parse the response from the API: {e}")
+        raise DeLijnAPIException from e
+
+    return halte
+
+
+# TODO: make parameters into ints
+def get_dienstregelingen(entiteitnummer: str, haltenummer: str) -> Dienstregeling:
+    """Get the schedule of the stop with the given entity and stop number
+
+    Returns:
+        Dienstregeling: The schedule of the stop with the given entity and stop number
+
+    Raises:
+        DeLijnAPIException: If the API request fails
+    """
+    result = _rest_adapter.get(
+        f"/haltes/{entiteitnummer}/{haltenummer}/dienstregelingen"
+    )
+    try:
+        assert result.data is not None
+        dienstregeling = Dienstregeling(
+            doorkomsten=[
+                Doorkomst(**clean_doorkomst(doorkomst))
+                for halte_doorkomsten in result.data["halteDoorkomsten"]
+                for doorkomst in halte_doorkomsten["doorkomsten"]
+            ],
+            doorkomst_notas=[
+                DoorkomstNota(**doorkomst_nota)
+                for doorkomst_nota in result.data["doorkomstNotas"]
+            ],
+            rit_notas=[RitNota(**rit_nota) for rit_nota in result.data["ritNotas"]],
+        )
+    except (AssertionError, ValidationError) as e:
+        _logger.error(f"Failed to parse the response from the API: {e}")
+        raise DeLijnAPIException from e
+
+    return dienstregeling
+
+
+def get_richtingen(entiteitnummer: int, haltenummer: int) -> list[Richting]:
+    """Get the directions of the stop with the given entity and stop number
+
+    Args:
+        entiteitnummer (int): The number of the entity
+        haltenummer (int): The number of the stop
+
+    Returns:
+        list[Richting]: The directions of the stop with the given entity and stop number
+
+    Raises:
+        DeLijnAPIException: If the API request fails
+    """
+    result = _rest_adapter.get(f"/haltes/{entiteitnummer}/{haltenummer}/lijnrichtingen")
+    try:
+        assert result.data is not None
+        richtingen = [
+            Richting(**richting) for richting in result.data["lijnrichtingen"]
+        ]
+    except (AssertionError, ValidationError) as e:
+        _logger.error(f"Failed to parse the response from the API: {e}")
+        raise DeLijnAPIException from e
+
+    return richtingen
+
+
+def get_omleidingen(entiteitnummer: int, haltenummer: int) -> list[Omleiding]:
+    """Get the directions of the stop with the given entity and stop number
+
+    Args:
+        entiteitnummer (int): The number of the entity
+        haltenummer (int): The number of the stop
+
+    Returns:
+        list[Richting]: The directions of the stop with the given entity and stop number
+
+    Raises:
+        DeLijnAPIException: If the API request fails
+    """
+    result = _rest_adapter.get(f"/haltes/{entiteitnummer}/{haltenummer}/omleidingen")
+    try:
+        assert result.data is not None
+        omleidingen = [
+            Omleiding(**omleiding) for omleiding in result.data["omleidingen"]
+        ]
+    except (AssertionError, ValidationError) as e:
+        _logger.error(f"Failed to parse the response from the API: {e}")
+        raise DeLijnAPIException from e
+
+    return omleidingen
+
+
+def get_real_time_doorkomsten(
+    entiteitnummer: int, haltenummer: int
+) -> list[RealTimeDoorkomst]:
+    """Get the real-time arrivals of the stop with the given entity and stop number
+
+    Args:
+        entiteitnummer (int): The number of the entity
+        haltenummer (int): The number of the stop
+
+    Returns:
+        list[RealTimeDoorkomst]: The real-time arrivals of the stop with the given entity and stop number
+
+    Raises:
+        DeLijnAPIException: If the API request fails
+    """
+    result = _rest_adapter.get(
+        f"/haltes/{entiteitnummer}/{haltenummer}/real-time-doorkomsten"
+    )
+    try:
+        assert result.data is not None
+        real_time_doorkomsten = [
+            RealTimeDoorkomst(**real_time_doorkomst)
+            for halteDoorkomst in result.data["halteDoorkomsten"]
+            for real_time_doorkomst in halteDoorkomst["doorkomsten"]
+        ]
+    except (AssertionError, ValidationError) as e:
+        _logger.error(f"Failed to parse the response from the API: {e}")
+        raise DeLijnAPIException from e
+
+    return real_time_doorkomsten
+
+
+def get_storingen(entiteitnummer: int, haltenummer: int) -> list[Storing]:
+    """Get the directions of the stop with the given entity and stop number
+
+    Args:
+        entiteitnummer (int): The number of the entity
+        haltenummer (int): The number of the stop
+
+    Returns:
+        list[Richting]: The directions of the stop with the given entity and stop number
+
+    Raises:
+        DeLijnAPIException: If the API request fails
+    """
+    result = _rest_adapter.get(f"/haltes/{entiteitnummer}/{haltenummer}/storingen")
+    try:
+        assert result.data is not None
+        storingen = [Storing(**storing) for storing in result.data["storingen"]]
+    except (AssertionError, ValidationError) as e:
+        _logger.error(f"Failed to parse the response from the API: {e}")
+        raise DeLijnAPIException from e
+
+    return storingen
--- lijnpy/models.py
+++ lijnpy/models.py
@@ -1,6 +1,6 @@
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Result(BaseModel):
@@ -17,6 +17,8 @@ class Result(BaseModel):
     data: dict[str, Any] | None = None
 
 
+# TODO: Remove by expanding models with extra functions
+# TODO: Move v1 models under kern_open_data_services_api.v1
 class Link(BaseModel):
     """Represents a link to a resource
 
@@ -53,6 +55,7 @@ class Gemeente(BaseModel):
 
     gemeentenummer: int
     omschrijving: str
+    # TODO: https://docs.pydantic.dev/latest/concepts/fields/#field-aliases
     hoofd_gemeente: HoofdGemeente | None = None
     links: list[Link]
 
@@ -193,3 +196,211 @@ class Vervoerregio(BaseModel):
     naam: str
     nr: str
     links: list[Link]
+
+
+class HalteInDeBuurt(BaseModel):
+    """Represents a stop in the neighborhood of a given geo-coordinate
+
+    Attributes:
+        type (str): The type of the stop
+        id (str): The ID of the stop
+        naam (str): The name of the stop
+        afstand (int): The distance to the stop
+        geo_coordinaat (GeoCoordinaat): The geographical coordinate of the stop
+        links (list[Link]): The links to other resources related to the stop
+    """
+
+    type: str
+    id: str
+    naam: str
+    afstand: int
+    geo_coordinaat: GeoCoordinaat
+    links: list[Link]
+
+
+class Doorkomst(BaseModel):
+    """Represents a passage of a vehicle at a stop
+
+    Attributes:
+        entiteitnummer (str): The number of the entity
+        lijnnummer (int): The number of the line
+        richting (str): The direction of the line
+        ritnummer (int): The number of the ride
+        bestemming (str): The destination of the ride
+        plaats_bestemming (str): The place of the destination
+        vias (str): The vias of the ride
+        dienstregeling_tijdstip (str): The schedule time of the ride
+        links (list[Link]): The links to other resources related to the passage
+    """
+
+    entiteitnummer: str
+    lijnnummer: int
+    richting: str
+    ritnummer: int
+    bestemming: str
+    plaats_bestemming: str
+    vias: list[str] | None = None
+    # TODO: Properly parse the datetime
+    dienstregeling_tijdstip: str
+    links: list[Link]
+
+
+class RitNota(BaseModel):
+    """Represents a note of a ride
+
+    Attributes:
+        id (int): The ID of the note
+        titel (str): The title of the note
+        ritnummer (int): The number of the ride
+        haltenummer (int): The number of the stop
+        omschrijving (str): The description of the note
+        entiteitnummer (int): The number of the entity
+        lijnnummer (int): The number of the line
+        richting (str): The direction of the line
+    """
+
+    id: int
+    titel: str
+    # TODO: Convert to int
+    ritnummer: str
+    haltenummer: str
+    omschrijving: str
+    entiteitnummer: int
+    lijnnummer: int
+    richting: str
+
+
+class DoorkomstNota(BaseModel):
+    """Represents a note of a passage of a vehicle at a stop
+
+    Attributes:
+        id (int): The ID of the note
+        titel (str): The title of the note
+        ritnummer (int): The number of the ride
+        haltenummer (int): The number of the stop
+        omschrijving (str): The description of the note
+        entiteitnummer (int): The number of the entity
+        lijnnummer (int): The number of the line
+        richting (str): The direction of the line
+    """
+
+    id: int
+    titel: str
+    ritnummer: str
+    haltenummer: int
+    omschrijving: str
+    entiteitnummer: int
+    lijnnummer: int
+    richting: str
+
+
+class Dienstregeling(BaseModel):
+    """Represents a schedule
+
+    Attributes:
+        doorkomsten (list[Doorkomst]): The passages of the schedule
+        doorkomst_notas (list[DoorkomstNota]): The notes of the passages of the schedule
+        rit_notas (list[RitNota]): The notes of the rides of the schedule
+        dienstregeling_notas (list[DienstregelingNota]): The notes of the schedule
+    """
+
+    doorkomsten: list[Doorkomst]
+    doorkomst_notas: list[DoorkomstNota]
+    rit_notas: list[RitNota]
+    # TODO: Add omleidingen
+
+
+class Richting(BaseModel):
+    """Represents a direction of a line
+
+    Attributes:
+        entiteitnummer (str): The number of the entity
+        lijnnummer (str): The number of the line
+        richting (str): The direction of the line
+        omschrijving (str): The description of the direction
+        links (list[Link]): The links to other resources related to the direction
+    """
+
+    entiteitnummer: str
+    lijnnummer: str
+    richting: str
+    omschrijving: str
+    links: list[Link]
+
+
+class OmleidingPeriode(BaseModel):
+    """Represents a period of a detour of a line
+
+    Attributes:
+        start_datum (str): The start date of the period
+        eind_datum (str, optional): The end date of the period. Defaults to None.
+    """
+
+    start_datum: str = Field(validation_alias="startDatum")
+    eind_datum: str | None = Field(default=None, validation_alias="eindDatum")
+
+
+class Omleiding(BaseModel):
+    """Represents a detour of a line
+
+    Attributes:
+        titel (str): The title of the detour
+        omschrijving (str): The description of the detour
+        periode (OmleidingPeriode): The period of the detour
+        referentie_omleiding (int): The reference ID of the detour
+        type (str): The type of the detour
+        omleidings_dagen (list[str]): The days of the week when the detour is active
+        links (list[Link]): The links to other resources related to the detour
+    """
+
+    titel: str
+    omschrijving: str
+    periode: OmleidingPeriode
+    # TODO: Add haltes en richtingen
+    referentie_omleiding: int = Field(validation_alias="referentieOmleiding")
+    type: str
+    omleidings_dagen: list[str] = Field(validation_alias="omleidingsDagen")
+    links: list[Link]
+
+
+class RealTimeDoorkomst(BaseModel):
+    """Represents a real-time passage of a vehicle at a stop
+
+    Attributes:
+        entiteitnummer (str): The number of the entity
+        lijnnummer (int): The number of the line
+        richting (str): The direction of the line
+        ritnummer (int): The number of the ride
+        bestemming (str): The destination of the ride
+        vias (list[str], optional): The vias of the ride. Defaults to None.
+        dienstregeling_tijdstip (str): The schedule time of the ride
+        real_time_tijdstip (str): The real-time time of the ride
+        vrtnum (int): The number of the real-time passage
+        prediction_statussen (list[str]): The status of the real-time passage
+        links (list[Link]): The links to other resources related to the real-time passage
+    """
+
+    entiteitnummer: str
+    lijnnummer: int
+    richting: str
+    # TODO: Search automatic pydantic conversion to int
+    ritnummer: str
+    bestemming: str
+    vias: list[str] | None = Field(default=None)
+    dienstregeling_tijdstip: str = Field(validation_alias="dienstregelingTijdstip")
+    real_time_tijdstip: str = Field(validation_alias="real-timeTijdstip")
+    vrtnum: str
+    prediction_statussen: list[str] = Field(validation_alias="predictionStatussen")
+    links: list[Link]
+
+
+class Storing(BaseModel):
+    """Represents a detour of a line
+
+    Attributes:
+        omschrijving (str): The description of the detour
+        lijnrichtingen (list[Richting]): The directions of the line
+    """
+
+    omschrijving: str
+    lijnrichtingen: list[Richting]
